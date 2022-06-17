c7ddecef58a6082a3ec3cd304e602c6ddb88c14b
wizard1066 <mark.lucking@gmail.com>
2022-06-17 10:32:04 +0200

    Initial Commit

---
+++ Foxy2Update/Foxy2UpdateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foxy2UpdateApp.swift
+//  Foxy2Update
+//
+//  Created by localuser on 17.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Foxy2UpdateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
